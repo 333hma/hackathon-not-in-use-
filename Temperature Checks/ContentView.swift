b8b1c6494e9eff58a570046ee35ce088c575196d
333hma <220544@hkis.edu.hk>
2021-03-07 00:28:15 +0800

    Initial Commit

---
+++ Temperature Checks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Temperature Checks
+//
+//  Created by Hayden MA on 7/3/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Temperature Checks/Temperature_ChecksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Temperature_ChecksApp.swift
+//  Temperature Checks
+//
+//  Created by Hayden MA on 7/3/2021.
+//
+
+import SwiftUI
+
+@main
+struct Temperature_ChecksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
